b41b9ec08876852463a88a2bd6c78eb8ae29c59e
ebenhamu <ebenhamu@gmail.com>
2025-03-27 21:53:36 +0200

    fix color and center

--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
 function showClock() {
     const clockContainer = document.getElementById("clock-container");
-    clockContainer.innerHTML = '<div style="border-radius:50%; width:150px; height:150px; background-color:grey; display:flex; align-items:center; justify-content:center;"><span id="time"></span></div>';
+    clockContainer.innerHTML = '<div id="clock"><span id="time"></span></div>';
     updateClock();
     setInterval(updateClock, 1000);
 }
